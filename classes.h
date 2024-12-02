3f0a7d1a7a73089c2afc006d934443004f4191b1
artemgon <artemhoncharuk123@gmail.com>
2024-12-02 10:47:24 +0200

    HW is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,87 @@
 #include "libs.h"
+
+class Reservoir
+{
+private:
+	string _name, _type;
+	float _volume, _area;
+public:
+	Reservoir() : _name("Unknown"), _type("Unknown"), _volume(0), _area(0) {}
+	explicit Reservoir(const string& name, const string& type, float volume,
+		float area) : _name(name), _type(type), _volume(volume), _area(area) {
+	}
+	Reservoir(const Reservoir& res1) : _name(res1._name), _type(res1._type), _volume(res1._volume), _area(res1._area) {}
+	float volFunc(float max_depth) const
+	{
+		return _area * max_depth;
+	}
+	bool checkFunc(const Reservoir& res1) const
+	{
+		return _type == res1._type;
+	}
+	bool compFunc(const Reservoir& res1) const
+	{
+		if (_type == res1._type)
+			return _area > res1._area;
+		else
+			return false;
+	}
+	void printFunc() const {
+		cout << endl;
+		cout << "Name: " << _name << endl;
+		cout << "Type: " << _type << endl;
+		cout << "Volume: " << _volume << endl;
+		cout << "Area: " << _area << endl;
+	}
+	const string& getName() const { return _name; }
+	const string& getType() const { return _type; }
+	float getV() const { return _volume; }
+	float getArea() const { return _area; }
+	void setName(const string& name) { _name = name; }
+	void setType(const string& type) { _type = type; }
+	void setV(float volume) { _volume = volume; }
+	void setArea(float area) { _area = area; }
+	~Reservoir() = default;
+};
+
+class ResArray
+{
+private:
+	Reservoir* reservoirs;
+	int size;
+public:
+	ResArray() : reservoirs(nullptr), size(0) {}
+	void addFunc(const Reservoir& res1)
+	{
+		Reservoir* newRes = new Reservoir[size + 1];
+		for (int i = 0; i < size; i++) {
+			*(newRes + i) = *(reservoirs + i);
+		}
+		newRes[size] = res1;
+		delete[] reservoirs;
+		reservoirs = newRes;
+		++size;
+	}
+	void deleteFunc(int index) {
+		if (index < 0 || index >= size) {
+			cout << "Error occurred... Choose valid index..." << endl;
+			return;
+		}
+		Reservoir* newRes= new Reservoir[size - 1];
+		for (int i = 0, j = 0; i < size; i++) {
+			if (i != index) {
+				newRes[j++] = reservoirs[i];
+			}
+		}
+		delete[] reservoirs;
+		reservoirs = newRes;
+		--size;
+	}
+	void printResFunc() const {
+		for (int i = 0; i < size; i++)
+			reservoirs[i].printFunc();
+	}
+	~ResArray() {
+		delete[] reservoirs;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,36 @@
 
 int main()
 {
+    ResArray array;
+    Reservoir r1("Oceanic", "ocean", 5000, 3000);
+    Reservoir r2("Lagoon", "sea", 1500, 800);
+    Reservoir r3("Swamp1", "swamp", 2000, 1000);
+    Reservoir r4("Swamp2", "swamp", 1500, 750);
+    Reservoir r5;
+    r5.setName("Sea");
+    r5.setType("sea");
+    r5.setV(2500);
+    r5.setArea(1000);
+    bool check = r3.checkFunc(r4);
+    bool comp = r3.compFunc(r4);
+    cout << endl;
+    cout << boolalpha;
+    cout << "It's " << check << " that r3 and r4 are the same types" << endl;
+    cout << endl;
+    cout << boolalpha;
+    cout << "It's " << comp << " that r3 is bigger than r4" << endl;
+    cout << noboolalpha;
+    string type = r3.getType();
+    cout << endl;
+    cout << "R3 type: " << type << endl;
+    array.addFunc(r1);
+    array.addFunc(r2);
+    array.addFunc(r3);
+    array.addFunc(r4);
+    cout << "All reservoirs:" << endl;
+    array.printResFunc();
+    array.deleteFunc(0);
+    cout << "After deleting :" << endl;
+    array.printResFunc();
 	return 0;
 }
